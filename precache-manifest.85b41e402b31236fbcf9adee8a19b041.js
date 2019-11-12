b24321546a9ac5be82d405e7ac4449fa92ccc73a
IvanVFedenko <ivan.v.fedenko@gmail.com>
2019-11-12 20:01:44 +0200

    Updates

---
+++ precache-manifest.85b41e402b31236fbcf9adee8a19b041.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddced56778998759922f24d4540eb6df",
+    "url": "/test_for_v-jet/index.html"
+  },
+  {
+    "revision": "5752da5acca03d27d828",
+    "url": "/test_for_v-jet/static/css/main.c148b557.chunk.css"
+  },
+  {
+    "revision": "c7f6ac1258388b3b6b83",
+    "url": "/test_for_v-jet/static/js/2.95b40edb.chunk.js"
+  },
+  {
+    "revision": "5752da5acca03d27d828",
+    "url": "/test_for_v-jet/static/js/main.9fe27a09.chunk.js"
+  },
+  {
+    "revision": "efc7ca12bff7e5db00d3",
+    "url": "/test_for_v-jet/static/js/runtime-main.2848ebf3.js"
+  }
+]);
